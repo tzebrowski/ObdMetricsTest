17b6fe98e9761ca2eff2aed144c9e7eebab220dc
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2025-01-13 11:09:37 +0100

    feat: Add spotless support. Update licence

--- src/main/java/org/obd/metrics/test/CodecTest.java
+++ src/main/java/org/obd/metrics/test/CodecTest.java
@@ -1,21 +1,19 @@
-/** 
- * Copyright 2019-2024, Tomasz Żebrowski
- * 
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ /**
+ * Copyright 2019-2025, Tomasz Żebrowski
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * <p>Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ */
 package org.obd.metrics.test;
 
 import java.util.Arrays;
@@ -29,6 +27,7 @@ import org.obd.metrics.codec.CodecRegistry;
 import org.obd.metrics.codec.batch.BatchCodec;
 import org.obd.metrics.codec.formula.FormulaEvaluatorConfig;
 import org.obd.metrics.command.obd.ObdCommand;
+import org.obd.metrics.context.Context;
 import org.obd.metrics.pid.PidDefinition;
 import org.obd.metrics.transport.message.ConnectorResponse;
 import org.obd.metrics.transport.message.ConnectorResponseFactory;
@@ -63,9 +62,12 @@ public interface CodecTest {
 		Assertions.assertThat(pid).isNotNull();
 		Assertions.assertThat(pidSource).isNotNull();
 		Assertions.assertThat(rawData).isNotNull();
-
-		final CodecRegistry codecRegistry = CodecRegistry.builder().formulaEvaluatorConfig(
-				FormulaEvaluatorConfig.builder().debug(debug).scriptEngine("JavaScript").build()).build();
+		
+		final CodecRegistry codecRegistry = CodecRegistry.
+				builder().context(Context.instance()).
+				formulaEvaluatorConfig(
+				FormulaEvaluatorConfig.builder(). 
+				debug(debug).scriptEngine("JavaScript").build()).build();
 
 		PidDefinition pidDef = null;
 		if (id == null) {
@@ -107,7 +109,10 @@ public interface CodecTest {
 		Assertions.assertThat(pidSource).isNotNull();
 		Assertions.assertThat(rawData).isNotNull();
 
-		final CodecRegistry codecRegistry = CodecRegistry.builder().formulaEvaluatorConfig(
+		final CodecRegistry codecRegistry = CodecRegistry
+				.builder()
+				.context(Context.instance())
+				.formulaEvaluatorConfig(
 				FormulaEvaluatorConfig.builder().debug(debug).scriptEngine("JavaScript").build()).build();
 
 		final PidDefinition pidDef = PIDsRegistryFactory.get(pidSource).findBy(pid);
